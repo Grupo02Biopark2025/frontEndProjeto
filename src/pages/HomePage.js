f2c854eca7e4cd0d9a2287850735922037b298c2
Gustavo Cortelassi <gutocortelassi72@gmail.com>
2025-06-03 18:53:41 -0300

    front end dashboard kpi

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import "../styles/Home.css";
+import DashboardKPI from "./DashboardKPI";
+
 
 function HomePage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -12,10 +14,12 @@ function HomePage() {
   return (
     <>
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className="content">
-            <h1>Bem-vindo ao CorpSync</h1>
-            <p>Selecione uma opção no menu lateral.</p>
-        </div>
+      <div className="content">
+        <h1>Bem-vindo ao CorpSync</h1>
+        <p>Selecione uma opção no menu lateral.</p>
+        <DashboardKPI />
+      </div>
+
     </>
   );
 }
